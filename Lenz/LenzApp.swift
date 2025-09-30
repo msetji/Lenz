cb1d0101116aabea547f5ffb3bacb97ea5af7820
Michael Setji <michaelpsetji@gmail.com>
2025-09-30 11:05:39 -0400

    Initial Commit

---
+++ Lenz/LenzApp.swift
@@ -0,0 +1,17 @@
+//
+//  LenzApp.swift
+//  Lenz
+//
+//  Created by Michael Setji on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct LenzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
